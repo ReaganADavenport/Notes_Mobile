267023ce04c9a4f8e97b801532a4536722a0fabb
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2020-08-18 15:14:23 -0400

    filling out the function of the home screen

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -31,6 +31,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].body
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //Show note controller
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {
+            return
+        }
+        vc.title = "Note"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
